b3f9f0625810eac56747cd8b94fe414f7f56f931
keith47733 <keith47733@gmail.com>
2022-10-08 06:48:12 -0400

    FINAL

---
+++ lib/qa.dart
@@ -0,0 +1,31 @@
+class qa {
+  static const questions = [
+    {
+      'questionText': 'What\'s your favourite colour?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favourite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+}
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'qa.dart';
 import './quiz_screen.dart';
 import './result.dart';
 
@@ -14,44 +15,14 @@ class _QuizState extends State<Quiz> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
-  static const _questions = [
-    {
-      'questionText': 'What\'s your favourite colour?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 3},
-        {'text': 'Snake', 'score': 11},
-        {'text': 'Elephant', 'score': 5},
-        {'text': 'Lion', 'score': 9},
-      ],
-    },
-    {
-      'questionText': 'Who\'s your favourite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-      ],
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Quiz')),
-      body: _questionIndex < _questions.length
+      body: _questionIndex < qa.questions.length
           ? QuizScreen(
               _questionIndex,
-              _questions,
+              qa.questions,
               _answerQuestion,
             )
           : Result(_totalScore, _resetQuiz),
